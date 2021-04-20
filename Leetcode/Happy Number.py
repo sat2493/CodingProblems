78863027548ebee4c30094de5748006be5ffaddd
sat2493 <satabligan@ucdavis.edu>
2021-04-20 00:01:11 -0700

    Update Happy Number.py

--- Leetcode/Happy Number.py
+++ Leetcode/Happy Number.py
@@ -5,16 +5,32 @@ import math
 class Solution:
     def isHappy(self, n: int) -> bool:
         
+        digits = str(n)
+        l = 0
+        for digit in digits:
+            l = l + int(digit) 
         
         while n != 1:
-            n_temp = n
             # Extract digits:
-            while n_temp != 0:
-                
-                # Update n_temp
-                n_temp = math.floor(n_temp / 10)
+            digits = str(n)
+             
+            # Update n
+            n = 0
+            for digit in digits:
+                # Square the digits
+                # Add each square to the sum
+                n = n + (int(digit) * int(digit))
+            print(n)
+            
+            # Adjust n
+            digits = str(n)
+            m = 0
+            for digit in digits:
+                m = m + int(digit)
+            print("Adjusted: ", m)
             
             # Case return to n
+            if m == l:
+                return False
             
-            # Update n
         return True
